4a5c5fac3416e3ebd3d663dbe108b75cd46e3fce
Jan-Simon Möller <dl9pf@gmx.de>
2024-12-06 12:29:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from machine import Pin
 from time import sleep
 
+epoch=0
+
 # Initialisierung von "LED" als Ausgang
 led_onboard = Pin("LED", Pin.OUT)
+
+import ugit
+ugit.pull_all()
 
 while True:
     # LED einschalten
     led_onboard.on()
     # 5 Sekunden warten
     sleep(1)
-
+    epoch=epoch+1
     # LED ausschalten
     led_onboard.off()
     # 5 Sekunden warten
     sleep(1)
+    epoch=epoch+1
+
+
+    # daily reboot
+    if epoch > 86400 :
+        machine.reset()
